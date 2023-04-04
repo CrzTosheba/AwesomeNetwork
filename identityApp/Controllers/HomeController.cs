a8c3d00c53c7409b7f1b674b56c70c91f7417f7a
CrzTosheba <fugu@list.ru>
2023-04-04 19:25:01 +0300

    Финальное

---
+++ identityApp/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using AwesomeNetwork.ViewModels.Account;
+using identityApp.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace identityApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(SignInManager<IdentityUser> signInManager, ILogger<HomeController> logger)
+        {
+            _signInManager = signInManager;
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            if (_signInManager.IsSignedIn(User))
+            {
+                return RedirectToAction("MyPage", "UserManagement");
+            }
+            else
+            {
+                return View(new MainViewModel());
+            }
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ identityApp/Hubs/ChatHub.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace AwesomeNetwork.Hubs
+{
+    public class ChatHub : Hub
+    {
+
+    }
+}
+ 
